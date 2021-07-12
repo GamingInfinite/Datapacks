335c0094cdc638fc90f3c62b31af1cd6d62a0b4e
GamingInfinite <ender2gamer123@gmail.com>
2021-07-12 11:46:56 -0500

    lost of things

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:objd/core.dart';
+import 'package:objd_crafting/objd_crafting.dart';
 
 void main(List<String> args) {
   createProject(
@@ -15,18 +16,74 @@ void main(List<String> args) {
           target: "./Compiled Datapacks/",
           generate: PendantWidget()),
       args);
+  createProject(
+      Project(
+          name: "Pendants Crafting",
+          version: 16,
+          target: "./Compiled Datapacks/",
+          generate: CraftingTable(
+              name: "pcraft",
+              main: [
+                Execute.asat(Entity(tags: ["pTablePlacer"]),
+                    children: [File.execute("set", create: false)]),
+                Kill(Entity(tags: ["pTablePlacer"]))
+              ],
+              displayName: createTextComponent("Pendant Table", packColors[0]),
+              placeholder:
+                  Item(Items.gray_stained_glass_pane, name: TextComponent("")),
+              blockModel: Item(Items.sheep_spawn_egg, model: 1, count: 1),
+              onDestroy: Summon(EntityType("item"),
+                  location: Location.rel(y: 0.7),
+                  nbt: {
+                    "Item": Item(Items.sheep_spawn_egg,
+                        name: createTextComponent("Pendant Table", Color.Gold,
+                            italic: false),
+                        count: 1,
+                        model: 1,
+                        nbt: {
+                          "EntityTag": Summon(EntityType("armor_stand"),
+                              tags: ["pTablePlacer"],
+                              nbt: {"Invisible": 1, "small": 1}).getMap()
+                        }).getMap()
+                  }),
+              recipes: [
+                Recipe(
+                    {
+                      2: Item(Items.prismarine_crystals),
+                      4: Item(Items.prismarine_shard),
+                      5: Item(Items.nautilus_shell),
+                      6: Item(Items.scute),
+                      8: Item(Items.heart_of_the_sea)
+                    },
+                    Item(Items.nautilus_shell,
+                        count: 1,
+                        name: createTextComponent("Dolphin Pendant", Color.Gold,
+                            italic: false),
+                        nbt: {
+                          "swiftswim": 1,
+                          "Enchantments": [
+                            {"id": "protection", "lvl": 0}
+                          ]
+                        },
+                        hideFlags: 1),
+                    name: "dolphin_pendant")
+              ])),
+      args);
 }
 
-TextComponent createTextComponent(String text, Color color) {
-  return TextComponent(text, color: color);
+TextComponent createTextComponent(String text, Color color, {bool italic}) {
+  return TextComponent(text, color: color, italic: italic);
 }
 
 List<Color> packColors = [
   Color.fromRGB(255, 204, 0),
   Color.fromRGB(252, 3, 244)
 ];
+
 Color AuthorColor = Color.fromRGB(108, 0, 232);
+
 Score butcher = Score(Entity.All(tags: ["isOp"]), "butcher");
+
 List<Widget> mainCUFile = [
   Scoreboard.trigger("butcher", addIntoLoad: true),
   Do.Until(Tag("butcherDisabled", entity: Entity.All()), then: [
@@ -37,7 +94,11 @@ List<Widget> mainCUFile = [
     ])
   ])
 ];
-List<Widget> mainPendantFile = [];
+
+List<Slot> PendantCheck = [Slot.MainHand, Slot.OffHand];
+List<Widget> mainPendantFile = [Storage.set("pcraft", key: key, value: value),For(create: (index) {
+  return If(Storage.get(name, key: "Storage"), then: then)
+}, to: PendantCheck.length)];
 
 class CUWidget extends Widget {
   @override
